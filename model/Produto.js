0f399ad9e7bdf3a21a74ef8a98af555fb3af5b53
Matheus-Clemente <136654905+Matheus-Clemente@users.noreply.github.com>
2025-03-03 11:24:15 -0300

    Update Produto.js

--- model/Produto.js
+++ model/Produto.js
@@ -1,6 +1,8 @@
+const { Schema, model } = require('mongoose');
+
 const produtoSchema = new Schema({
     nome: { type: String, required: true },
-    descricao: {String, required: true},
+    descricao: {type:String, required: true},
     preco: { type: Number, required: true },
     categoria: { type: String, required: true },
     estoque: { type: Number, default: 0 }

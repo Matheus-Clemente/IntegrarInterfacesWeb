2ac786dc39cca25f4e5e39798d4d3764fe6a7f99
Matheus-Clemente <136654905+Matheus-Clemente@users.noreply.github.com>
2025-03-12 20:36:33 -0300

    Update Produto.js

--- model/Produto.js
+++ model/Produto.js
@@ -1,23 +1,14 @@
-// Importa o Schema e model do Mongoose para definir o modelo de dados
 const { Schema, model } = require('mongoose');
 
-// Define o schema para o modelo de Produto
 const produtoSchema = new Schema({
-  // Nome do produto, obrigatório
-  nome: { type: String, required: true },
-
-  // Descrição do produto, obrigatória
-  descricao: { type: String, required: true },
-
-  // Preço do produto, obrigatório
-  preco: { type: Number, required: true },
-
-  // Categoria do produto (ex: alimentos, bebidas), obrigatória
+  info: { type: Schema.Types.ObjectId, ref: 'Info', required: true }, 
+  descricao: {type: String, required:true},
   categoria: { type: String, required: true },
+  estoque: { type: Number, default: 0 },
+  pedidos: [{
+        pedido: { type: Schema.Types.ObjectId, ref: 'Pedido' },
+        quantidade: { type: Number, default: 1 }
+      }],
+}, { timestamps: true });
 
-  // Quantidade em estoque, valor padrão 0 caso não seja especificado
-  estoque: { type: Number, default: 0 }
-}, { timestamps: true }); // Adiciona timestamps de criação e atualização automaticamente
-
-// Cria e exporta o modelo de Produto
 module.exports = model('Produto', produtoSchema);
